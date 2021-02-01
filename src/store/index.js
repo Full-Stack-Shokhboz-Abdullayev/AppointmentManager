f64815c7dba0bd086e93b58c037f45f96c25fb92
Shokhboz Abdullaev <abdullaevadiana27@gmail.com>
2021-02-01 13:06:45 +0300

    'little-bug-fixed'

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
 		dialogAppointment: {},
 		appointments: [
 			{
-				_id: uniqueId++,
+				_id: (uniqueId += 1),
 				fullName: `App${uniqueId}`,
 				description: "Should be done.",
 				date: "2020-01-01",
@@ -44,15 +44,15 @@ export default new Vuex.Store({
 				description: "Should be done.",
 				date: "2020-01-01",
 				done: false
-			},
+			}
 		]
 	},
 	mutations: {
 		changeAppointmentStraight({ appointments }, payload) {
 			const index = appointments.findIndex((a) => a._id === payload._id)
-      console.log(payload, index)
+			console.log(payload, index)
 
-      //detect any changes in payload.newData and accordingly change actual value in state
+			//detect any changes in payload.newData and accordingly change actual value in state
 			Object.keys(payload.newData).forEach((p) => {
 				Vue.set(appointments[index], p, payload.newData[p])
 			})
@@ -60,19 +60,20 @@ export default new Vuex.Store({
 		deleteAppointmentStraight({ appointments }, _id) {
 			const index = appointments.findIndex((a) => a._id === _id)
 			Vue.delete(appointments, index)
-    },
-    addAppointmentStraight(state, payload) {
-      const data = {...payload,  _id: uniqueId++}
-      state.appointments.unshift(data)
-    },
-    changeDialog(state, {bool, _id}) {
-      if (_id) {
-        const val = state.appointments.find((a) => a._id === _id)
-        state.dialogAppointment = val
-      }
-      console.log(state.dialogAppointment);
-      state.dialogOpened = bool
-    }
+		},
+		addAppointmentStraight(state, payload) {
+			const data = { ...payload, _id: uniqueId++ }
+			state.appointments.unshift(data)
+		},
+		changeDialog(state, { bool, _id }) {
+			if (_id) {
+				const val = state.appointments.find((a) => a._id === _id)
+				console.log(val)
+				state.dialogAppointment = val
+			}
+			console.log(state.dialogAppointment)
+			state.dialogOpened = bool
+		}
 	},
 	actions: {
 		changeAppointment({ commit }, payload) {
@@ -80,10 +81,10 @@ export default new Vuex.Store({
 		},
 		deleteAppointment({ commit }, payload) {
 			commit("deleteAppointmentStraight", payload)
-    },
-    addAppointment({commit}, payload) {
-      commit("addAppointmentStraight", payload)
-    }
+		},
+		addAppointment({ commit }, payload) {
+			commit("addAppointmentStraight", payload)
+		}
 	},
 	modules: {}
 })
